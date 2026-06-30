89984571dbc8e441203b5f46f7ad8aefa02c0756
Zeliha Bodur <zelihabodur12@gmail.com>
2026-06-30 12:00:10 +0300

    1. gün variables ve input

---
+++ variables.py
@@ -0,0 +1,21 @@
+isim = "Zeliha"
+yas = "20"
+boy = "1.65"
+ogrenci = "True"
+
+print(isim)
+print(yas)
+print(boy)
+print(True)
+
+isim = "Zeliha"
+yas = "20"
+
+print("Merhaba", isim)
+print("Yaşım", yas)
+
+isim = input("Adini gir: ")
+yas = input("yasini gir: ")
+
+print("Merhaba", isim)
+print("sen", yas, "yasindasin")
