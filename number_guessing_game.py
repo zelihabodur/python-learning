6d828955b4184f4f37662d4d5f77617086159b0f
Zeliha Bodur <zelihabodur12@gmail.com>
2026-07-02 23:24:52 +0300

    Improve number guessing game with range validation

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -6,15 +6,25 @@ print("Sayi Tahmin Oyununa Hosgeldiinnn!")
 print("1 ile 20 arasinda bir sayi tuttuumm")
 print("5 tahmin hakkin var. Basarilarr")
 
+deneme_sayisi = 0
+
 while tahmin_hakki > 0:
     tahmin = int(input("Tahminin "))
+  
+    if tahmin < 1 or tahmin > 20:
+        print("Lutfen 1 ile 20 arasinda bir sayi gir.")
+        continue
+    
+    deneme_sayisi += 1
 
     if tahmin == gizli_sayi:
         print("Tebriklerr, Dogru Bildinnn")
+        print("Deneme sayin:", deneme_sayisi)
         break
 
     elif tahmin < gizli_sayi:
         print("Daha buyuk bir sayi denemelisinn")
+    
     else:
         print("Daha kucuk bir sayi denemelisinn")
 
@@ -22,5 +32,5 @@ while tahmin_hakki > 0:
     print("Kalan hakkin:", tahmin_hakki)
 
 if tahmin_hakki == 0:
-    print("Hakkin Bitti:()")
+    print("Hakkin Bitti:(")
     print("Gizli Sayi:", gizli_sayi)
